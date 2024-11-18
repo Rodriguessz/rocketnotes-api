df8f1b9dafaebb77d3efb5b7eaf0ee74bae04b4a
Enzo <enzo.orodrigues03@gmail.com>
2024-11-18 16:02:30 -0300

    Adicionando rota para visualizar status do server

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 //Roteador principal - Centraliza as rotas da aplicação
 
 //Funcionalidade Router permite definir rotas de forma modular
-const { Router } = require("express");
+const { Router, response } = require("express");
 
 //Inicializa uma instância de Router
 const router = Router();
@@ -13,6 +13,7 @@ const tagsRouter = require("./tags.routes");
 const sessionsRouter = require("./sessions.routes")
 
 //Declarando rotas mães para recursos especificos - Callback será o carregamento do router especifico
+router.use("/", (request, response) => response.json({message: "Server on!"}))
 router.use("/users", userRouter);
 router.use("/notes", notesRouter);
 router.use("/tags", tagsRouter);
