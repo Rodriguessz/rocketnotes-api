80262ef6695a05e93a29cd4dc6df764bf62024f8
Enzo <enzo.orodrigues03@gmail.com>
2024-12-12 11:13:18 -0300

    COnfigurando e testando jest

---
+++ src/mytest.spec.js
@@ -0,0 +1,14 @@
+
+//It(DescricaoTeste, callback) - Função utilizada para escrever um teste em jest.
+it("Expected receive 2 numbers and the sum between bothr must be 4 ", () => {
+    //Variables
+    const a = 2;
+    const b = 2;
+
+    const result = a + b;
+
+    //Expect - Função utilizada para verificar se o resultado esperado é igual ao resultado obtido.
+    expect(result).toEqual(4);
+
+});
+
